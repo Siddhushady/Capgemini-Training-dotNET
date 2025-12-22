e56e93be9e5d4553f7afb1bf70331179903c5b6a
Siddhushady <siddhuduke3@gmail.com>
2025-12-22 17:27:29 +0530

    Add methods level 1 problems

---
+++ Methods/Level 1/Level 1/ChocolateDistribution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Chocolates
+{
+    static int[] FindRemainderAndQuotient(int chocolates, int children)
+    {
+        return new int[] { chocolates / children, chocolates % children };
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter number of chocolates: ");
+        int chocolates = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter number of children: ");
+        int children = Convert.ToInt32(Console.ReadLine());
+
+        int[] result = FindRemainderAndQuotient(chocolates, children);
+        Console.WriteLine($"Each child gets {result[0]} chocolates and remaining {result[1]}");
+    }
+}
---
+++ Methods/Level 1/Level 1/PosNeg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class NumberCheck
+{
+    static int CheckNumber(int number)
+    {
+        if (number > 0) return 1;
+        if (number < 0) return -1;
+        return 0;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Result: {CheckNumber(number)}");
+    }
+}
